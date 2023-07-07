5a1be77f1a15260db957ff3099dc1920f260c2e9
Dominic Tracy <spoon@Dominics-MacBook-Air.local>
2023-07-07 00:45:40 -0500

    added a new page

---
+++ src/app/petlisting/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PetListing = () => {
+  return (
+    <div className="bg-primary py-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-white text-4xl font-bold">Find A Friend Today</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PetListing;
